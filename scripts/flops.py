703065ed0ac55d66c20bba6a03202488197b27f8
billl-jiang <jb972617930@gmail.com>
2023-03-06 13:52:21 +0800

    Add FLOPs scripts

---
+++ scripts/flops.py
@@ -0,0 +1,140 @@
+import json
+import os
+from pathlib import Path
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from rich import get_console
+from rich.table import Table
+from omegaconf import OmegaConf
+from thop import profile
+from fvcore.nn import FlopCountAnalysis, parameter_count_table
+from mld.callback import ProgressLogger
+from mld.config import parse_args
+from mld.data.get_data import get_datasets
+from mld.models.get_model import get_model
+from mld.utils.logger import create_logger
+
+
+def print_table(title, metrics):
+    table = Table(title=title)
+
+    table.add_column("Metrics", style="cyan", no_wrap=True)
+    table.add_column("Value", style="magenta")
+
+    for key, value in metrics.items():
+        table.add_row(key, str(value))
+
+    console = get_console()
+    console.print(table, justify="center")
+
+
+def get_metric_statistics(values, replication_times):
+    mean = np.mean(values, axis=0)
+    std = np.std(values, axis=0)
+    conf_interval = 1.96 * std / np.sqrt(replication_times)
+    return mean, conf_interval
+
+
+def main():
+    # parse options
+    cfg = parse_args(phase="test")  # parse config file
+    cfg.FOLDER = cfg.TEST.FOLDER
+    # create logger
+    logger = create_logger(cfg, phase="test")
+    output_dir = Path(
+        os.path.join(cfg.FOLDER, str(cfg.model.model_type), str(cfg.NAME),
+                     "samples_" + cfg.TIME))
+    output_dir.mkdir(parents=True, exist_ok=True)
+    logger.info(OmegaConf.to_yaml(cfg))
+
+    # set seed
+    pl.seed_everything(cfg.SEED_VALUE)
+
+    # gpu setting
+    if cfg.ACCELERATOR == "gpu":
+        # os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(
+        #     str(x) for x in cfg.DEVICE)
+        os.environ["PYTHONWARNINGS"] = "ignore"
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    # create dataset
+    datasets = get_datasets(cfg, logger=logger, phase="test")[0]
+    logger.info("datasets module {} initialized".format("".join(
+        cfg.TRAIN.DATASETS)))
+
+    # create model
+    model = get_model(cfg, datasets)
+    logger.info("model {} loaded".format(cfg.model.model_type))
+    
+
+    # optimizer
+    metric_monitor = {
+        "Train_jf": "recons/text2jfeats/train",
+        "Val_jf": "recons/text2jfeats/val",
+        "Train_rf": "recons/text2rfeats/train",
+        "Val_rf": "recons/text2rfeats/val",
+        "APE root": "Metrics/APE_root",
+        "APE mean pose": "Metrics/APE_mean_pose",
+        "AVE root": "Metrics/AVE_root",
+        "AVE mean pose": "Metrics/AVE_mean_pose",
+    }
+
+    # callbacks
+    callbacks = [
+        pl.callbacks.RichProgressBar(),
+        ProgressLogger(metric_monitor=metric_monitor),
+    ]
+    logger.info("Callbacks initialized")
+
+    # trainer
+    trainer = pl.Trainer(
+        benchmark=False,
+        max_epochs=cfg.TRAIN.END_EPOCH,
+        accelerator=cfg.ACCELERATOR,
+        devices=list(range(len(cfg.DEVICE))),
+        default_root_dir=cfg.FOLDER_EXP,
+        reload_dataloaders_every_n_epochs=1,
+        log_every_n_steps=cfg.LOGGER.LOG_EVERY_STEPS,
+        deterministic=False,
+        detect_anomaly=False,
+        enable_progress_bar=True,
+        logger=None,
+        callbacks=callbacks,
+    )
+
+    # loading state dict
+    logger.info("Loading checkpoints from {}".format(cfg.TEST.CHECKPOINTS))
+
+    state_dict = torch.load(cfg.TEST.CHECKPOINTS,
+                            map_location="cpu")["state_dict"]
+    model.load_state_dict(state_dict)
+
+    macs_lst = []
+    params_lst = []
+    flops_lst = []
+    for (i, batch) in enumerate(datasets.test_dataloader()):
+      print('batch size', len(batch['text']))
+      macs, params = profile(model, (batch,))
+      # flops = FlopCountAnalysis(model, batch).total()
+      # print(flops)
+      # flops_lst.append(flops)
+      print('macs', macs/1e9)
+      print('flops', macs*2/1e9)
+      return
+      macs_lst.append(macs)
+      params_lst.append(params)
+
+      if len(flops_lst) == 1:
+        break
+    
+    print(macs_lst)
+    print(params_lst)
+    # print(np.mean(flops_lst)/1e9)
+    print(np.mean(macs_lst)/1e9)
+    print(np.mean(params_lst)/1e6)
+
+
+if __name__ == "__main__":
+    main()
